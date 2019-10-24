a06422f8df0f59701103d4a80308d8e45ecafdbf
sophiefz16 <andayaarnelbryant@gmail.com>
2019-10-24 16:20:52 +0700

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     HeaderComponent
   ],
   imports: [
